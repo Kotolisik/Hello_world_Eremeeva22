ded4a76a9f35688d7dae001f59b30918dac72851
Eremeeva Taisia Dmitrievna 4731204/50003 <e889rm@mail.ru>
2026-03-06 23:41:47 +0300

    Add files via upload

---
+++ projects_3/task_3_4/task_3-4_1_1.py
@@ -0,0 +1,19 @@
+1ake.cif
+1hew.cif
+4ake.cif
+anaconda3
+Desktop
+Documents
+Downloads
+lin
+Music
+Pictures
+Public
+task_2-0_1.py
+task_3-4_1_1.py
+Templates
+test
+vbox-build.log
+Videos
+VirtualBox VMs
+Снимки экрана
---
+++ projects_3/task_3_4/task_3-4_1_2.py
@@ -0,0 +1,49 @@
+ls -lah
+
+total 1.1M
+drwx------. 1 mrrr mrrr  808 Feb 25 16:49 .
+drwxr-xr-x. 1 root root    8 Jan 22 22:26 ..
+-rw-r--r--. 1 mrrr mrrr 417K Feb  7 15:21 1ake.cif
+-rw-r--r--. 1 mrrr mrrr 168K Feb  7 14:10 1hew.cif
+-rw-r--r--. 1 mrrr mrrr 373K Feb  7 15:22 4ake.cif
+drwxr-xr-x. 1 mrrr mrrr  502 Feb 25 16:46 anaconda3
+-rw-------. 1 mrrr mrrr  25K Feb 25 16:48 .bash_history
+-rw-r--r--. 1 mrrr mrrr   18 Jul 23  2025 .bash_logout
+-rw-r--r--. 1 mrrr mrrr  144 Jul 23  2025 .bash_profile
+-rw-r--r--. 1 mrrr mrrr  993 Feb  6 22:35 .bashrc
+drwx------. 1 mrrr mrrr  892 Feb 23 19:24 .cache
+drwxr-xr-x. 1 mrrr mrrr   50 Feb  6 22:35 .conda
+-rw-r--r--. 1 mrrr mrrr   42 Feb  7 13:34 .condarc
+drwxr-xr-x. 1 mrrr mrrr  704 Feb 25 08:32 .config
+drwxr-xr-x. 1 mrrr mrrr    0 Feb 18 19:17 Desktop
+drwxr-xr-x. 1 mrrr mrrr   66 Feb 23 19:27 Documents
+drwxr-xr-x. 1 mrrr mrrr 1.1K Feb 25 09:03 Downloads
+-rwxr-xr--. 1 mrrr mrrr   96 Feb 21 14:30 .fehbg
+drwxr-xr-x. 1 mrrr mrrr    0 Feb 19 15:38 .gphoto
+drwxr-xr-x. 1 mrrr mrrr   30 Feb  7 13:14 .ipython
+drwxr-xr-x. 1 mrrr mrrr   22 Feb 21 14:07 .jupyter
+drwxrwxrwx. 1 mrrr mrrr   46 Jan 22 18:21 lin
+drwx------. 1 mrrr mrrr   20 Jan 22 22:26 .local
+drwxr-xr-x. 1 mrrr mrrr   48 Jan 22 22:30 .mozilla
+drwxr-xr-x. 1 mrrr mrrr    0 Feb  5 01:59 Music
+drwxr-xr-x. 1 mrrr mrrr   10 Feb  7 13:11 .npm
+drwxr-xr-x. 1 mrrr mrrr   72 Jan 30 16:10 Pictures
+drwx------. 1 mrrr mrrr   10 Feb  5 03:21 .pki
+drwxr-xr-x. 1 mrrr mrrr    0 Feb  5 01:59 Public
+-rw-r--r--. 1 mrrr mrrr   10 Feb  6 22:45 .pymoltimestamp
+-rw-------. 1 mrrr mrrr 4.4K Feb 22 15:17 .python_history
+-rw-------. 1 mrrr mrrr   51 Feb  5 01:38 .serverauth.107034
+-rw-r--r--. 1 mrrr mrrr   22 Feb 25 15:57 task_2-0_1.py
+-rw-r--r--. 1 mrrr mrrr  199 Feb 25 16:47 task_3-4_1_1.py
+-rw-r--r--. 1 mrrr mrrr    0 Feb 25 16:49 task_3-4_1_2.py
+drwxr-xr-x. 1 mrrr mrrr    0 Feb  5 01:59 Templates
+drwxr-xr-x. 1 mrrr mrrr    8 Feb 23 20:24 test
+drwxr-xr-x. 1 mrrr mrrr   18 Feb  5 06:05 .Trash-1000
+drwxr-xr-x. 1 mrrr mrrr    6 Feb  4 23:42 .var
+-rw-r--r--. 1 mrrr mrrr  591 Feb 18 07:44 vbox-build.log
+drwxr-xr-x. 1 mrrr mrrr    0 Feb  5 01:59 Videos
+drwxr-xr-x. 1 mrrr mrrr   14 Feb 18 20:19 VirtualBox VMs
+-rw-------. 1 mrrr mrrr  161 Feb  5 01:38 .Xauthority
+-rw-r--r--. 1 mrrr mrrr    8 Feb  5 02:34 .xinitrc
+drwxr-xr-x. 1 mrrr mrrr  220 Feb 25 08:37 .zen
+drwxr-xr-x. 1 mrrr mrrr 1.2K Feb  5 01:35 Снимки экрана
